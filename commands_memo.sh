cfd9b96f2d22b7263db71dcf120ef5954f67c43a
ZAKARIA BOUZOUBA <z.bouzouba@mundiapolis.ma>
2026-01-04 05:05:40 +0100

    Modify Zabbix agent installation commands in script

--- commands_memo.sh
+++ commands_memo.sh
@@ -6,15 +6,17 @@ sudo usermod -aG docker $USER
 
 # --- 2. Démarrage Zabbix ---
 docker compose up -d
-docker compose ps
+docker ps
 
 # --- 3. Client Linux (Agent) ---
-sudo add-apt-repository universe
+wget https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_7.0-2+ubuntu24.04_all.deb
+sudo dpkg -i zabbix-release_7.0-2+ubuntu24.04_all.deb
 sudo apt update
 sudo apt install -y zabbix-agent
 # Edit config: sudo nano /etc/zabbix/zabbix_agentd.conf
 # Change: Server=10.0.1.X
 sudo systemctl restart zabbix-agent
+sudo systemctl enable zabbix-agent
 
 # --- 4. Client Windows ---
 # Installer MSI -> IP Serveur: 10.0.1.X -> Port 10050
